1bceb9c0c9a4945aaf54cd639a6b2859eabfeb59
Chirag Ferwani <chiragferwani@gmail.com>
2023-11-07 09:34:45 +0530

    add files

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+
+const Header = () => {
+  return (
+    <div className="gpt3__header section__padding">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something To Ensure Your Digital Privacy
+        </h1>
+        <p className="" align="justify">
+        VPN Sneak is a comprehensive online privacy and security project and provides  an Android app, the Sneak2.0 hardware device, and an educational website to empower users with accessible online privacy and security solutions.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1600+ Visitors in last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
